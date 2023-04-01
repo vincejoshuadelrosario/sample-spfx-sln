46d1c571830b81661d0999a7866b37cebed25389
vince.j.del.rosario <vince.j.del.rosario@accenture.com>
2023-04-02 02:18:11 +0800

    DragDrop: Implement simple drag and drop example

--- src/module-02/src/webparts/dragDrop/components/DragDrop.tsx
+++ src/module-02/src/webparts/dragDrop/components/DragDrop.tsx
@@ -1,11 +1,54 @@
 import * as React from 'react';
 import styles from './DragDrop.module.scss';
 import { IDragDropProps } from './IDragDropProps';
+import { useRef, useState } from 'react';
 
 const DragDrop: React.FC<IDragDropProps> = () => {
+  const dragItem = useRef<number>();
+  const dragOverItem = useRef<number>();
+
+  const [list, setList] = useState<string[]>(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5',]);
+  
+  const onItemDrag = (e: React.DragEvent<HTMLDivElement>, position: number): void => {
+    dragItem.current = position;
+  };
+ 
+  const onItemEnter = (e: React.DragEvent<HTMLDivElement>, position: number): void => {
+    dragOverItem.current = position;
+  };
+
+  const onItemDrop = (): void => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent); 
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
+  };
+
+  const onItemOver = (e: React.DragEvent<HTMLDivElement>): void => {
+    e.preventDefault();
+  };
 
   return (<section className={styles.dragDrop}>
-  </section>)
+    <div className={styles.list}>
+    {
+      list &&
+      list.map((item, index) => (
+        <div className={styles.listItem}
+          onDragStart={(e) => onItemDrag(e, index)}
+          onDragEnter={(e) => onItemEnter(e, index)}
+          onDragOver={(e) => onItemOver(e)}
+          onDragEnd={() => onItemDrop()}
+          key={index}
+          draggable>
+            {item}
+        </div>
+      )) 
+    }
+    </div>
+  </section>);
 }
 
 export default DragDrop;
