44e41b37b3b9091eee05cf082e4f78fbf139d288
vince.j.del.rosario <vince.j.del.rosario@accenture.com>
2023-04-03 00:41:42 +0800

    Add SPFx solution for Module-03

---
+++ src/module-03/src/webparts/webPart01/components/IWebPart01Props.ts
@@ -0,0 +1,7 @@
+export interface IWebPart01Props {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/module-03/src/webparts/webPart01/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IWebPart01WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'WebPart01WebPartStrings' {
+  const strings: IWebPart01WebPartStrings;
+  export = strings;
+}
