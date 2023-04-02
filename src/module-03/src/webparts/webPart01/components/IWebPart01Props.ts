096a9a784e50ce16e7da7eff3b4c385d0f243eba
vince.j.del.rosario <vince.j.del.rosario@accenture.com>
2023-04-03 01:49:38 +0800

    DynamicData: Clean up scaffolded files

--- src/module-03/src/webparts/webPart01/components/IWebPart01Props.ts
+++ src/module-03/src/webparts/webPart01/components/IWebPart01Props.ts
@@ -1,7 +1,2 @@
 export interface IWebPart01Props {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
 }
--- src/module-03/src/webparts/webPart02/components/IWebPart02Props.ts
+++ src/module-03/src/webparts/webPart02/components/IWebPart02Props.ts
@@ -1,7 +1,2 @@
 export interface IWebPart02Props {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
 }
